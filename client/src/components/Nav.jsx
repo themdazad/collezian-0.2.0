abfb12f667e0716a3091d15416057e381b964638
modest-azad <collezian@gmail.com>
2024-05-07 19:38:10 +0530

    browser taskbar color added

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -9,7 +9,9 @@ export default function Nav() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
+    // shouldHideOnScroll
+    
+    <Navbar  onMenuOpenChange={setIsMenuOpen}>
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
