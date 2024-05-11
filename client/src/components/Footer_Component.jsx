8d0223b43157e426f40ba1ed064f5a8d7b5581ff
modest-azad <collezian@gmail.com>
2024-05-11 10:25:03 +0530

    .

--- client/src/components/Footer_Component.jsx
+++ client/src/components/Footer_Component.jsx
@@ -22,7 +22,7 @@ export function Footer_Component() {
         </div>
         <FooterDivider />
         <FooterCopyright href="#" by="Collezian" year={currentYear} />
-        <p>Designed and Developed with <span className="text-red-600">❤</span> by <a className=" text-sky-600" target="_blank" href="https://www.instagram.com/the_mdazad/">Azad</a></p>
+        <p className="text-sm">Designed and Developed with <span className="text-red-600">❤</span> by <a className=" text-sky-600" target="_blank" href="https://www.instagram.com/the_mdazad/">Azad</a></p>
       </div>
     </Footer>
   );
