d71e0b6eacaeaada611c1ff819ff1c6b69da1f47
modest-azad <collezian@gmail.com>
2024-05-18 09:24:01 +0530

    Update home page heading and button text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
         <>
             <div className="w-full min-h-[400px] grid justify-center content-center " >
                 <div className="text-center">
-                    <h2 className="md:text-[56px] text-[48px] max-sm:pt-12 font-bold lg:mb-12 uppercase">Join <br/>Community&apos;s Community</h2>
+                    <h2 className="md:text-[56px] text-[48px] max-sm:pt-12 font-bold lg:mb-12 uppercase">Join <br/>Engineer&apos;s Community</h2>
                     <Button className="items-center text-sm max-w-min py-6  rounded-2xl" color="primary" variant="flat" startContent={<GoPeople />}>
-                        <Link href="https://chat.whatsapp.com/HmEXaa6y1zE1Rdi60n7VA5" target="_blank">Join here</Link>
+                        <Link href="https://chat.whatsapp.com/HmEXaa6y1zE1Rdi60n7VA5" target="_blank">Join Community</Link>
                     </Button>
                 </div>
             </div>
