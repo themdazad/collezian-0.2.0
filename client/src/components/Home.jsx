eae72548199c338bb5a9ac5f33bcdb496b7130f0
modest-azad <collezian@gmail.com>
2024-05-18 09:23:11 +0530

    refactor: Update home page layout and button styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
         <>
             <div className="w-full min-h-[400px] grid justify-center content-center " >
                 <div className="text-center">
-                    <h2 className="md:text-[56px] text-[48px] font-bold lg:mb-12 uppercase">Join <br/>Community&apos;s Community</h2>
-                    <Button className="items-center text-sm max-w-min py-6 max-sm:pt-12 rounded-2xl" color="primary" variant="flat" startContent={<GoPeople />}>
+                    <h2 className="md:text-[56px] text-[48px] max-sm:pt-12 font-bold lg:mb-12 uppercase">Join <br/>Community&apos;s Community</h2>
+                    <Button className="items-center text-sm max-w-min py-6  rounded-2xl" color="primary" variant="flat" startContent={<GoPeople />}>
                         <Link href="https://chat.whatsapp.com/HmEXaa6y1zE1Rdi60n7VA5" target="_blank">Join here</Link>
                     </Button>
                 </div>
