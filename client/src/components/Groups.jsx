1c322966e1b6a3226f9e27b1f50a7a707db2173e
modest-azad <collezian@gmail.com>
2024-06-06 09:00:50 +0530

    .

--- client/src/components/Groups.jsx
+++ client/src/components/Groups.jsx
@@ -8,13 +8,27 @@ const Groups = () => {
       <Image src="/gif/people.gif" height={50} width={50}/>
       Recommended Groups
       </header>
-      <div className="groups flex justify-center my-8 gap-8">
+      <div className="snap-mandatory snap-x scrollbar-hide overflow-scroll groups flex justify-center my-6 gap-8 ">
 
-      <Image src="/Groups_Image/collezian_dsa.jpg" height={80} width={80}/>
-      <Link href="https://t.me/collezian_bcece_le">
+      
+      <Link className="snap-center min-w-[50px]" href="https://t.me/collezian_bcece_le">
+      <Image  src="/Groups_Image/collezian_dsa.jpg" height={80} width={80}/>
+      </Link>
+
+      <Link className="snap-center min-w-[50px]" href="https://t.me/collezian_bcece_le">
       <Image src="/Groups_Image/bcece_le_quiz.png" height={80} width={80}/>
       </Link>
+
+      <Link className="snap-center min-w-[50px]" href="https://t.me/collezian_bcece_le">
       <Image src="/Groups_Image/off_campus_placements.jpg" height={80} width={80}/>
+      </Link>
+      
+      <Link className="snap-center min-w-[50px]" href="https://t.me/collezian_bcece_le">
+      <Image src="/Groups_Image/off_campus_placements.jpg" height={80} width={80}/>
+      </Link>
+    
+      
+
       </div>
     </section>
     </>
