8a5923461d8a2e64c552a530e8aa2d89b0bf101b
modest-azad <collezian@gmail.com>
2024-06-08 18:33:11 +0530

    event card changed

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -6,23 +6,15 @@ import { MdOutlineArrowRight } from "react-icons/md";
 const event_data = [
     {image:"/events/techkshitizThumbnail.jpg",
         tittle:"TechKshitiz '24",
-        description:"Last Date - 30 June 2024",
+        description:"First ever technical event organizing by Government Engineering College, Siwan",
+        last_date:"Last Date - 30 June 2024",
         registration_link:"https://techkshitiz.in/government-engineering-college-siwan/total/events/list",
     },
-       
-    {image:"/logo/Collezian.svg",
+    
+    {image:"/events/techkshitizThumbnail.jpg",
         tittle:"",
-        description:"",
-        registration_link:"",
-    },
-    {image:"/logo/Collezian.svg",
-        tittle:"",
-        description:"",
-        registration_link:"",
-    },
-    {image:"/logo/Collezian.svg",
-        tittle:"",
-        description:"",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ducimus.",
+        last_date:"Not Announced",
         registration_link:"",
     },
     
@@ -49,17 +41,18 @@ const Syllabus = () => {
                 {/* <p className="px-4 my-4 text-sm text-justify CardTittle lg:text-center">We aim to be your one-stop destination for all things related to Engineering. Here&apos;s what you can expect to find on our platform.</p> */}
 
 
-                <div className="snap-mandatory snap-x flex scrollbar-hide overflow-scroll  px-[5%]  gap-4 py-6 ">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-[5%]  gap-4 py-6 ">
                     {
                         event_data.map((event,i) => (
-                            <div key={i} className="shadow-md hover:shadow-lg transition-shadow duration-350  snap-center min-w-[300px] max-w-[240px] h-full  gap-8 m-auto  bg-white/5 border border-zinc-300/40 rounded-3xl  overflow-clip"> 
-                              <Image className="z-0 object-cover aspect-[4/3] w-full" src={event.image}  radius="none" alt="" />
+                            <div key={i} className="shadow-md hover:shadow-lg transition-shadow duration-350  min-w-[250px] h-full  gap-8 m-auto  bg-white/5 border border-zinc-300/40 rounded-3xl  overflow-clip"> 
+                              {/* <Image className="z-0 object-cover aspect-[4/3] w-full" src={event.image}  radius="none" alt="" /> */}
                                 <div className="flex flex-col w-full m-auto p-[5%]">
-                                    <h1 className="font-bold uppercase">{event.tittle.length === 0 ? "Exited !" :event.tittle} </h1>
-                                    <p className="text-sm ">{event.description.length === 0 ? "For the events in Bihar" :event.description}</p>
+                                    <h1 className="font-bold text-xl uppercase">{event.tittle.length === 0 ? "Coming Soon" :event.tittle} </h1>
+                                    <p className="text-sm">{event.description.length === 0 ? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, ducimus." :event.description}</p>
+                                    <h3 className=" text-sm my-1 text-danger">{event.tittle.length === 0 ? "---" :event.last_date} </h3>
                                     <Link href={event.registration_link} target="_blank" className=" text-sm cursor-pointer" color="primary" variant="flat"
                                     >
-                                        {event.registration_link.length === 0 ? "Coming Soon" : "Register Now"} <MdOutlineArrowRight />
+                                        {event.tittle.length === 0 ? "Add Your College Event" : "Register Now"} <MdOutlineArrowRight />
                                     </Link>
                                 </div>
                             </div>
