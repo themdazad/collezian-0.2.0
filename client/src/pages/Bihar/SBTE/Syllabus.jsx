e89408251a250eabd56cf1ad0d137545ef8bf383
modest-azad <collezian@gmail.com>
2024-06-10 22:38:02 +0530

    bihar materials page fixed

--- client/src/pages/Bihar/SBTE/Syllabus.jsx
+++
@@ -1,9 +0,0 @@
-import { FiPaperclip } from "react-icons/fi"
-export const SBTE_Syllabus = () => {
-  return (
-    <div className="py-6 px-[5%]">
-    <p className="text-3xl font-bold text-center CardTittle text-sky-600 flex justify-center"><FiPaperclip/>Syllabus</p>
-    <h2 className="text-center font-semibold text-sky-600">State Board of Technical Education, Patna</h2>
-  </div>
-  )
-}
