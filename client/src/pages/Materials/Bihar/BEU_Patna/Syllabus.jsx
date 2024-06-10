5b330ee5d9d51d49f4652157c9c51764ea7c13cf
modest-azad <collezian@gmail.com>
2024-06-10 23:10:30 +0530

    accordion added in figma

--- client/src/pages/Materials/Bihar/BEU_Patna/Syllabus.jsx
+++ client/src/pages/Materials/Bihar/BEU_Patna/Syllabus.jsx
@@ -1,30 +1,36 @@
-import { useState } from "react";
-import { FiPaperclip } from "react-icons/fi";
+import {Accordion, AccordionItem} from "@nextui-org/react";
 export const BEU_Syllabus = () => {
-  const [hidden, setHidden] = useState(true);
-  function toggle() {
-    if (hidden == true) {
-      setHidden(false);
-    } else setHidden(true);
-  }
+  const defaultContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+ 
   return (
     <>
-      <section className="py-6 px-[5%]">
-        <p className="text-3xl font-bold text-center CardTittle text-sky-600 flex justify-center">
-          <FiPaperclip />
-          Syllabus
-        </p>
-        <h2 className="text-center font-semibold text-sky-600">
-          Bihar Engineering University, Patna
-        </h2>
-      </section>
-      <section>
-        <div className=" bg-gray-200 p-4 " onClick={toggle}>
-          <h2>Electrical Engineering</h2>
-          <div className={hidden ? "hidden" : ""}>
-            <p>Dropdown Item</p>
-          </div>
-        </div>
+      <center>
+        <h1 className=" text-2xl font-bold " >BEU B.Tech Syllabus</h1>
+        <p className="text-sm text-gray-500 mb-4 px-[10%] ">Bihar Engineering University, Patna</p>
+      </center>
+
+      <section className="px-[5%] md:px-[10%]">
+      <Accordion>
+      <AccordionItem key="1" aria-label="Accordion 1" subtitle="Press to expand" title="Electrical Engineering">
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem
+        key="2"
+        aria-label="Accordion 2"
+        subtitle={
+          <span>
+            Press to expand and download syllabus from semester 1 to 8
+          </span>
+        }
+        title="Mechanical Engineering"
+      >
+        {defaultContent}
+      </AccordionItem>
+      <AccordionItem key="3" aria-label="Accordion 3" subtitle="Press to expand" title="Civil Engineering">
+        {defaultContent}
+      </AccordionItem>
+    </Accordion>
       </section>
     </>
   );
